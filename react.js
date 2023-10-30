965ce73088a5ac44de73fef11d3c80cfd59e1d4a
DevPedroHB <pedroh.bergamo20@gmail.com>
2023-10-30 13:50:22 -0300

    🎉 feat: Commit inicial

---
+++ react.js
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@tanstack/eslint-plugin-query/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "standard",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@tanstack/query", "@typescript-eslint", "react"],
+  rules: {
+    "@tanstack/query/exhaustive-deps": "error",
+    "@tanstack/query/prefer-query-object-syntax": "error",
+    "@tanstack/query/stable-query-client": "error",
+    "linebreak-style": ["error", "unix"],
+    indent: ["error", 2],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/parsers": {
+      [require.resolve("@typescript-eslint/parser")]: [".ts", ".tsx", ".d.ts"],
+    },
+  },
+  ignorePatterns: ["node_modules"],
+};
